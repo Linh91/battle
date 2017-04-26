3ea5d897eac6ae4f35e2831da51b6060e8300ea8
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-04-26 20:33:35 +0100

    viewing player_two HP

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,10 +1,18 @@
-feature 'Testing infrastructure' do
+feature 'Battle game' do
 
-  scenario 'entering names' do
+  scenario 'entering players' do
     visit('/')
     fill_in('player_one', with: 'Jeremy')
     fill_in('player_two', with: 'Adam')
     click_button('Submit')
     expect(page).to have_content('Jeremy vs Adam for the championship!')
+  end
+
+  scenario 'player two HP points' do
+    visit('/')
+    fill_in('player_one', with: 'Jeremy')
+    fill_in('player_two', with: 'Adam')
+    click_button('Submit')
+    expect(page).to have_content('60/60 HP')
   end
 end
