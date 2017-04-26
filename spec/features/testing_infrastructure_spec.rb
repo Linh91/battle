7cc1b56ee4a62dde4411fb9730563f16e5d127dc
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-04-26 20:43:37 +0100

    sign_in method

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,18 +1,12 @@
 feature 'Battle game' do
 
   scenario 'entering players' do
-    visit('/')
-    fill_in('player_one', with: 'Jeremy')
-    fill_in('player_two', with: 'Adam')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Jeremy vs Adam for the championship!')
   end
 
   scenario 'player two HP points' do
-    visit('/')
-    fill_in('player_one', with: 'Jeremy')
-    fill_in('player_two', with: 'Adam')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('60/60 HP')
   end
 end
